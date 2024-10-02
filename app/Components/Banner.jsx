511631ff27e89dec82b6e7c0f61126aabc90d834
itsmayurijain <mayurijainsongir@gmail.com>
2024-10-02 17:00:40 +0530

    banner updated

--- app/Components/Banner.jsx
+++ app/Components/Banner.jsx
@@ -3,28 +3,28 @@ import React from 'react'
 export const Banner = () => {
   return (
     <div><section className="bg-gray-50">
-    <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center">
-      <div className="mx-auto max-w-xl text-center">
-        <h1 className="text-3xl font-extrabold sm:text-5xl">
-          Understand User Flow.
-          <strong className="font-extrabold text-purple-700 sm:block"> Increase Conversion. </strong>
-        </h1>
-  
-        <p className="mt-4 sm:text-xl/relaxed">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-          numquam ea!
-        </p>
-  
-        <div className="mt-8 flex flex-wrap justify-center gap-4">
-          <a
-            className="block w-full rounded bg-purple-700 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-            href="#"
-          >
-            Get Started
-          </a>
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center">
+        <div className="mx-auto text-center">
+          <h1 className="text-6xl font-extrabold  text-purple-700">
+            AI Course Generator
+          </h1>
+          <strong className="font-extrabold text-5xl text-black pt-4 sm:block">Custom Learning Paths, Powered by AI </strong>
+
+
+          <p className="mt-4 sm:text-xl/relaxed">
+            Unlock Personalized Education AI-Driven course creation.Tailor your learning <br />journey to fit your unique goals and pace
+          </p>
+
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+              className="block w-full rounded mt-2 bg-purple-700 px-12 py-3 text-lg font-medium text-white shadow hover:bg-black focus:outline-none focus:ring active:bg-black sm:w-auto"
+              href="#"
+            >
+              Get Started
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </section></div>
+    </section></div>
   )
 }
